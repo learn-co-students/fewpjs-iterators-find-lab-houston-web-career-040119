fb6b1ab5019796e5d1f7469852d079baff834ab2
jpavioli <justin.avioli@gmail.com>
2019-05-21 15:56:14 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let winning = record.find( obj => obj.result == "W")
+  if (winning != undefined) {return winning.year}
+  else {return undefined}
+}
