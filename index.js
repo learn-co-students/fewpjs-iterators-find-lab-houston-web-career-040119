5f84a8ab4ecb2904c0c8adc989bb53a5f852f56d
PYN <yxp7531540@gmail.com>
2019-05-22 13:26:28 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  if (record.find(resultOfYear => resultOfYear.result === 'W')) {
+    return record.find(resultOfYear => resultOfYear.result === 'W').year 
+  }
+}
