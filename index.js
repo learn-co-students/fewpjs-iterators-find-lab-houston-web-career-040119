b0905b209acb5aede0862bcf36e1389a05633437
shawn0527 <shawnren527@gmail.com>
2019-05-21 21:56:31 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let record = array.find(element => element.result === 'W')
+  return !!record ? record.year : undefined
+}
